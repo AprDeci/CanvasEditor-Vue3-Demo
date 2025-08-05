de6228a5c65cf79dc661a5f26cd23df09a349e66
aprdec <1263868407@qq.com>
2025-08-06 07:45:27 +0800

    添加CanvasEditor保存功能并显示保存内容

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
   import CanvasEditor from './canvasEditor/index.vue';
+  import { ref } from 'vue';
+
+  const html = ref('');
+
+  const Save = (json) => {
+    html.value = json;
+  };
 </script>
 
 <template>
-  <div><CanvasEditor :edit-mode="'edit'" /></div>
+  <div><CanvasEditor @save="Save" :edit-mode="'edit'" /></div>
+  <div style="height: 20rem">123:{{ html }}</div>
 </template>
